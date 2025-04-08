f9243bd655b82b31324fa01f9cab29f8a220354a
Prajwal Shrestha <pjmessi25@icloud.com>
2025-04-08 20:43:17 +0545

    Add comments for ts-jest

--- lua/hanger/test_actions/typescript.lua
+++ lua/hanger/test_actions/typescript.lua
@@ -1,6 +1,26 @@
 local Typescript = {}
 local javascript = require("hanger.test_actions.javascript")
 
+-- For typescript, make sure ts-jest is also installed with proper config.
+-- Config example: in package.json (taken from NestJS's package.json)
+-- "jest": {
+--   "moduleFileExtensions": [
+--     "js",
+--     "json",
+--     "ts"
+--   ],
+--   "rootDir": "src",
+--   "testRegex": ".*\\.spec\\.ts$",
+--   "transform": {
+--     "^.+\\.(t|j)s$": "ts-jest"
+--   },
+--   "collectCoverageFrom": [
+--     "**/*.(t|j)s"
+--   ],
+--   "coverageDirectory": "../coverage",
+--   "testEnvironment": "node"
+-- }
+
 function Typescript.execute_single(config)
     javascript.execute_single(config, true)
 end
